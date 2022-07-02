bebf1643435a37377f5fa04a47b4e858df297574
bartoszbilejczyk <bartosz.bilejczyk@refurbed.com>
2022-07-02 13:26:56 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="p-safe max-h-fit h-screen w-screen">
+  <div class="p-safe max-h-fit h-screen w-screen app">
     <div class="h-full"><router-view /></div>
     <Menu />
   </div>
 </template>
 
 <script setup lang="ts">
-  import Menu from '@/components/Menu.vue';
+  import Menu from './components/Menu.vue';
 </script>
 
-<style></style>
+<style>
+  .app {
+    padding: env(safe-inset-area-top) env(safe-inset-area-right) env(safe-inset-area-bottom) env(safe-inset-area-left) !important;
+  }
+</style>
