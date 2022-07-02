8bef56152230600d8688c312bee720684618ef83
bartoszbilejczyk <bartosz.bilejczyk@refurbed.com>
2022-07-02 13:28:46 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-safe max-h-fit h-screen w-screen app">
+  <div class="px-6 max-h-fit h-screen w-screen app">
     <div class="h-full"><router-view /></div>
     <Menu />
   </div>
@@ -8,9 +8,3 @@
 <script setup lang="ts">
   import Menu from './components/Menu.vue';
 </script>
-
-<style>
-  .app {
-    padding: env(safe-inset-area-top) env(safe-inset-area-right) env(safe-inset-area-bottom) env(safe-inset-area-left) !important;
-  }
-</style>
