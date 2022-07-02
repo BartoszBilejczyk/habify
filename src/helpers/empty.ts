8adf62fc18c252e4da0e7d1ce3fe44d67edccd09
bartoszbilejczyk <bartosz.bilejczyk@refurbed.com>
2022-07-02 15:45:28 +0200

    update

---
+++ src/helpers/empty.ts
@@ -0,0 +1,113 @@
+import {
+  User,
+  UserBasic,
+  Friend,
+  Challenge,
+  Charity,
+  Task,
+  Notification,
+  Invitation,
+  InvitationForChallenge,
+  ChallengeExample
+} from '../types';
+
+export const emptyUser: User = {
+  id: '',
+  email: '',
+  phone: '',
+  name: '',
+  points: 0,
+  image: '',
+  friends: [],
+  notifications: [], // TODO check if it should be here
+  notificationSettings: [],
+  tasks: [],
+  challenges: [],
+  feedbackSent: []
+};
+
+export const emptyUserBasic: UserBasic = {
+  id: '',
+  email: '',
+  phone: '',
+  name: '',
+  points: 0,
+  image: ''
+};
+
+export const emptyFriend: Friend = {
+  id: '',
+  email: '',
+  phone: '',
+  name: '',
+  addedDate: ''
+};
+
+export const emptyChallenge: Challenge = {
+  id: '',
+  inviter: { ...emptyUserBasic },
+  invitee: { ...emptyUserBasic },
+  winner: { ...emptyUserBasic },
+  loser: { ...emptyUserBasic },
+  title: '',
+  durationInDays: '',
+  type: null,
+  betCategory: null,
+  betDetails: null, // TODO
+  points: '',
+  inviteLink: '',
+  proposedChanges: null, // TODO
+  status: null,
+  confirmationPhotos: [],
+  confirmationType: null
+};
+
+export const emptyCharity: Charity = {
+  id: '',
+  logo: '',
+  name: '',
+  link: '',
+  description: '',
+  image: '',
+  category: ''
+};
+
+export const emptyTask: Task = {
+  id: '',
+  name: '',
+  description: '',
+  points: 0,
+  expirationDate: '',
+  finishDate: '',
+  category: 'other',
+  status: 'active'
+};
+
+export const emptyNotification: Notification = {
+  id: '',
+  name: '',
+  description: '',
+  points: 0,
+  category: 'other',
+  status: 'active'
+};
+
+export const emptyInvitation: Invitation = {
+  id: '',
+  referralCode: '',
+  invitee: { ...emptyUserBasic },
+  inviter: { ...emptyUserBasic }
+};
+
+export const emptyInvitationForChallenge: InvitationForChallenge = {
+  ...emptyInvitation,
+  challenge: { ...emptyChallenge }
+};
+
+export const emptyChallengeExample: ChallengeExample = {
+  id: '',
+  title: '',
+  durationInDays: '',
+  type: null,
+  points: ''
+};
---
+++ src/types/index.ts
@@ -0,0 +1,112 @@
+export interface User {
+  id: string;
+  email: string;
+  phone: string;
+  name: string;
+  points: number;
+  image: string;
+  friends: Friend[];
+  notifications: Notification[]; // TODO check if it should be here
+  notificationSettings: string[];
+  tasks: Task[];
+  challenges: Challenge[];
+  feedbackSent: string[];
+}
+
+export interface UserBasic {
+  id: string;
+  email: string;
+  phone: string;
+  name: string;
+  points: number;
+  image: string;
+}
+
+export interface Friend {
+  id: string;
+  email: string;
+  phone: string;
+  name: string;
+  addedDate: string;
+}
+
+export type ChallengeStatus = 'waitsForConfirm' | 'done' | 'pending' | 'active' | 'closed';
+export type ChallengeType = 'oneTime' | 'duration';
+export type ChallengeConfirmationType = 'manual' | 'automatic';
+export type ChallengeCategory = 'social' | 'financial' | 'sport' | 'other';
+
+export interface Challenge {
+  id: string;
+  inviter: UserBasic;
+  invitee: UserBasic;
+  winner: UserBasic;
+  loser: UserBasic;
+  title: string;
+  durationInDays: string;
+  type: ChallengeType | null;
+  betCategory: ChallengeCategory | null; // TODO add more
+  betDetails: any; // TODO
+  points: string;
+  inviteLink: string;
+  proposedChanges: any; // TODO
+  status: ChallengeStatus | null;
+  confirmationPhotos: string[];
+  confirmationType: ChallengeConfirmationType | null;
+}
+
+export type CharityCategory = 'kids' | 'animals' | 'environment' | 'other';
+
+export interface Charity {
+  id: string;
+  logo: string;
+  name: string;
+  link: string;
+  description: string;
+  image: string;
+  category: CharityCategory | '';
+}
+
+export type TaskStatus = 'active' | 'rewarded' | 'expired';
+export type TaskCategory = 'friends' | 'challenge' | 'other'; // TODO
+
+export interface Task {
+  id: string;
+  name: string;
+  description: string;
+  points: number;
+  expirationDate: string;
+  finishDate: string;
+  category: TaskCategory;
+  status: TaskStatus;
+}
+
+export type NotificationStatus = 'active' | 'seen';
+export type NotificationCategory = 'friends' | 'challenge' | 'other'; // TODO
+
+export interface Notification {
+  id: string;
+  name: string;
+  description: string;
+  points: number;
+  category: NotificationCategory;
+  status: NotificationStatus;
+}
+
+export interface Invitation {
+  id: string;
+  referralCode: string;
+  invitee: UserBasic;
+  inviter: UserBasic;
+}
+
+export interface InvitationForChallenge extends Invitation {
+  challenge: Challenge;
+}
+
+export interface ChallengeExample {
+  id: string;
+  title: string;
+  durationInDays: string;
+  type: ChallengeType | null;
+  points: string;
+}
